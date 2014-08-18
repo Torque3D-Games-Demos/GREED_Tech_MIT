0efda85b55832b843b92b420352658a7fe62f28d
J0linar <j0linartube@gmail.com>
2014-08-18 17:58:45 +0200

    added Vignette PostFx Shader
    Vignette PostFx added to the postFxManager

---
+++ Templates/Full/game/core/scripts/client/postFx/vignette.cs
@@ -0,0 +1,28 @@
+// Vignette PostFx
+$VignettePostEffect::RadiusX = 0.6;
+$VignettePostEffect::RadiusY = 0.2;
+
+singleton ShaderData( VignetteShader )
+{   
+   DXVertexShaderFile 	= "shaders/common/postFx/postFxV.hlsl";
+   DXPixelShaderFile 	= "shaders/common/postFx/vignette/VignetteP.hlsl";
+   pixVersion = 2.0;
+};
+
+singleton PostEffect( VignettePostEffect )  
+{  
+   isEnabled         = false;
+   allowReflectPass  = false;
+   renderTime        = "PFXAfterBin";
+   renderBin         = "GlowBin";
+   shader            = VignetteShader;
+   stateBlock        = PFX_DefaultStateBlock;
+   texture[0]        = "$backBuffer";
+   renderPriority    = 10;
+};
+
+function VignettePostEffect::setShaderConsts(%this)
+{
+   %this.setShaderConst("$radiusX", $VignettePostEffect::RadiusX);
+   %this.setShaderConst("$radiusY", $VignettePostEffect::RadiusY);
+}
